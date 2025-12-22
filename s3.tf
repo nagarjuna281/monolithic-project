98fa2f309d88879bed23452e4f17a085e47a4ff0
nagarjuna281 <kamalenagarjuna@gmail.com>
2025-12-22 15:42:19 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik1122.monobucket"
+  bucket = "arjun12345.monobucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "mustafashaik1122.monobucket"
+bucket = "arjun12345.monobucket"
 key = "prod/terraform.tfstate"
 }
 }
